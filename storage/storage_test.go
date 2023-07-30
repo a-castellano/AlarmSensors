5a3799b3a84bb78b7fbe18329cc4b1153f427341
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2023-07-30 10:01:08 +0000

    Finnish storage tests for the time being

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -7,7 +7,7 @@ import (
 	redismock "github.com/go-redis/redismock/v8"
 )
 
-func TestNewsReadEmptySet(t *testing.T) {
+func TestReadEmptySet(t *testing.T) {
 	db, mock := redismock.NewClientMock()
 
 	var key string = "ab123"
@@ -18,10 +18,58 @@ func TestNewsReadEmptySet(t *testing.T) {
 
 	changed, err := storageInstance.UpdateAndNotify(ctx, key, true)
 	if err != nil {
-		t.Error("TestNewsReadEmptySet, should not fail, error was ", err.Error())
+		t.Error("TestReadEmptySet, should not fail, error was ", err.Error())
 	}
 	if changed != true {
-		t.Error("TestNewsReadEmptySet, changed should be true as no previos record was stored.")
+		t.Error("TestReadEmptySet, changed should be true as no previous record was stored.")
+	}
+
+}
+
+func TestNoChanged(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+
+	expectedValues := make(map[string]string)
+	expectedValues["name"] = "ab123"
+	expectedValues["lastupdated"] = "123"
+	expectedValues["triggered"] = "false"
+
+	var key string = "ab123"
+	mock.ExpectHGetAll(key).SetVal(expectedValues)
+
+	storageInstance := Storage{db}
+	var ctx = context.TODO()
+
+	changed, err := storageInstance.UpdateAndNotify(ctx, key, false)
+	if err != nil {
+		t.Error("TestNoChanged, should not fail, error was ", err.Error())
+	}
+	if changed != false {
+		t.Error("TestNoChanged, changed should be false as previous record has not changed.")
+	}
+
+}
+
+func TestChanged(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+
+	expectedValues := make(map[string]string)
+	expectedValues["name"] = "ab123"
+	expectedValues["lastupdated"] = "123"
+	expectedValues["triggered"] = "false"
+
+	var key string = "ab123"
+	mock.ExpectHGetAll(key).SetVal(expectedValues)
+
+	storageInstance := Storage{db}
+	var ctx = context.TODO()
+
+	changed, err := storageInstance.UpdateAndNotify(ctx, key, true)
+	if err != nil {
+		t.Error("TestNoChanged, should not fail, error was ", err.Error())
+	}
+	if changed != true {
+		t.Error("TestNoChanged, changed should be true as previous record was changed.")
 	}
 
 }
